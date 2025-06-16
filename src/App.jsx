53ba71faa9a3250c1d90610b2253fe13664e24ec
Abhishek16oS <Abhishek16business@protonmail.com>
2025-06-16 15:30:08 +0530

    sign up Form  here

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+/* import React, { useActionState } from 'react'
+
+const App = () => {
+
+
+  
+}
+
+export default App
+ */
+
+import React from 'react'
+import SignupForm from './compoennt/SignUp'
+import Form from './compoennt/Form'
+
+const App = () => {
+  return (
+    <div className='min-h-screen py-2'>
+      {/* <SignupForm/> */}
+      <Form/>
+    </div>
+  )
+}
+
+export default App
